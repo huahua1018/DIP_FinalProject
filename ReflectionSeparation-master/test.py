6815cf668382eef913a7aefaa4d5cc9d2f1c33c6
huahua1018 <shelly10182433@gmail.com>
2024-12-10 23:36:20 +0800

    modify train test of baseline

--- ReflectionSeparation-master/test.py
+++ ReflectionSeparation-master/test.py
@@ -6,14 +6,33 @@ import torch.optim as optim
 from model import NetArticle
 import Image_DataSet as dtst
 import cv2
+import os
+import errno
+import shutil
 
 
-hyper_params = {
-    'indoor_size': 20,
-    'outdoor_size': 100,
-    'input_size': (3, 128, 128),
-    'num_epochs': 10,
-    'learning_rate': 0.001
+
+def create_folder(path):
+    try:
+        os.mkdir(path)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
+        pass
+
+def del_folder(path):
+    try:
+        shutil.rmtree(path)
+    except OSError as exc:
+        pass
+
+hyper_params = { # TODO
+    'train1_size': 45, #20
+    'train2_size': 45, #100
+    'input_size': (3, 256, 256),
+    'save_folder': './results',
+    # 'num_epochs': 10, 
+    # 'learning_rate': 0.001 
 }
 
 
@@ -25,7 +44,7 @@ def get_batch(batch):
     return features, target_transpose, target_reflection
 
 
-def test(test_loader, model, criterion):
+def test(test_loader, model, criterion, path):
     losses = []
     model.eval()
     step = 0
@@ -46,9 +65,12 @@ def test(test_loader, model, criterion):
                     print(a[i][j][0], file=writer)
             '''
             print("-------------------------------")
-            #cv2.imwrite("imgs/target_trans" + str(i) + ".png", (np.transpose(target_transmission[0].detach().numpy(), (1, 2, 0)) * 255).astype(int))
-            #cv2.imwrite("imgs/transmission" + str(i) + ".png", (np.transpose(predict_transmission[0].detach().numpy(), (1, 2, 0)) * 255).astype(int))
-            #cv2.imwrite("imgs/reflection" + str(i) + ".png", (np.transpose(predict_reflection[0].detach().numpy(), (1, 2, 0)) * 255).astype(int))
+            
+        for j in range(predict_transmission.shape[0]):    # TODO: check this
+            cv2.imwrite(path + "/target_trans" + str(i) + ".png", (np.transpose(target_transmission[j].detach().numpy(), (1, 2, 0)) * 255).astype(int))
+            cv2.imwrite(path + "/transmission" + str(i) + ".png", (np.transpose(predict_transmission[j].detach().numpy(), (1, 2, 0)) * 255).astype(int))
+            cv2.imwrite(path + "/reflection" + str(i) + ".png", (np.transpose(predict_reflection[j].detach().numpy(), (1, 2, 0)) * 255).astype(int))
+
         loss1 = criterion(predict_transmission, target_transmission)
         loss2 = criterion(predict_reflection, target_reflection)
         loss = loss1 + loss2
@@ -61,12 +83,17 @@ def test(test_loader, model, criterion):
 if __name__ == "__main__":
     print(th.__version__)
 
-    data = dtst.ImageDataSet(hyper_params['indoor_size'], hyper_params['outdoor_size'])
+    path = hyper_params['save_folder']
+    del_folder(path)
+    create_folder(path)
+
+
+    data = dtst.ImageDataSet(hyper_params['train1_size'], hyper_params['train2_size'])
     test_loader = dtst.DataLoader(data, 1, 18, test=True)
 
     #net = NetArticle()
-    net = th.load("weights3.hdf5")
+    net = th.load("weights3.hdf5") # TODO
     criterion = nn.MSELoss()
-    losses = test(test_loader, net, criterion)
+    losses = test(test_loader, net, criterion, path)
     print('####', losses)
 
--- ReflectionSeparation-master/train.py
+++ ReflectionSeparation-master/train.py
@@ -10,12 +10,12 @@ import Image_DataSet as dtst
 import config
 
 
-experiment = Experiment(api_key=config.comet_ml_api,
-                        project_name="reflection-separation", workspace="wibbn")
+# experiment = Experiment(api_key=config.comet_ml_api,
+#                         project_name="reflection-separation", workspace="wibbn")
 
-hyper_params = {
-    'indoor_size': 5,
-    'outdoor_size': 25,
+hyper_params = { # TODO
+    'train1_size': 12, #5
+    'train2_size': 12, #125
     'input_size': (3, 128, 128),
     'batch_size': 4,
     'num_epochs': 10,
@@ -31,53 +31,50 @@ def get_batch(batch):
 
 
 def train(train_loader, model, criterion, optimizer, epochs=hyper_params['num_epochs'], save=True, device=th.device("cuda")):
-    with experiment.train():
+    # with experiment.train():
+    losses = []
+    model.train()
+    step = 0
+    for epoch in range(epochs):
+        # experiment.log_current_epoch(epoch)
         losses = []
-        model.train()
-        step = 0
-        for epoch in range(epochs):
-            experiment.log_current_epoch(epoch)
-            losses = []
-            for i, batch in enumerate(train_loader):
-                features, target_transmission, target_reflection = get_batch(batch)
-                features = features.to(device)
-                target_transmission = target_transmission.to(device)
-                optimizer.zero_grad()
-                #predict_transmission, predict_reflection = model(features)
-                predict_transmission = model(features)
-                print('__________________________________')
-                #print(predict_transmission[0])
-                #print(target_transmission[0] - predict_transmission[0])
-                loss = criterion(predict_transmission, target_transmission)
-                #loss2 = criterion(predict_reflection, target_reflection)
-                #loss = loss1 + loss2
-                #print("LOSSES: ", loss1, loss)
-                loss.backward()
-                optimizer.step()
-                print(epoch, step, loss.item(), th.mean(model.conv_down_6.weight.grad[0][0]), th.mean(model.conv_intro_2.weight.grad[0][0]))
-                experiment.log_metric('loss', loss.item(), step=step)
-                step += 1
-                losses.append(loss.item())
-                if save:
-                    th.save(model, 'weights3.hdf5')
-            print('epoch end', sum(losses))
-        return losses
+        for i, batch in enumerate(train_loader):
+            features, target_transmission, target_reflection = get_batch(batch)
+            features = features.to(device)
+            target_transmission = target_transmission.to(device)
+            optimizer.zero_grad()
+            #predict_transmission, predict_reflection = model(features)
+            predict_transmission = model(features)
+            print('__________________________________')
+            #print(predict_transmission[0])
+            #print(target_transmission[0] - predict_transmission[0])
+            loss = criterion(predict_transmission, target_transmission)
+            #loss2 = criterion(predict_reflection, target_reflection)
+            #loss = loss1 + loss2
+            #print("LOSSES: ", loss1, loss)
+            loss.backward()
+            optimizer.step()
+            print(epoch, step, loss.item(), th.mean(model.conv_down_6.weight.grad[0][0]), th.mean(model.conv_intro_2.weight.grad[0][0]))
+            # experiment.log_metric('loss', loss.item(), step=step)
+            step += 1
+            losses.append(loss.item())
+            if save:
+                th.save(model, 'weights3.hdf5')
+        print('epoch end', sum(losses))
+    return losses
 
 
 if __name__ == "__main__":
     print(th.__version__)
-    experiment.log_parameters(hyper_params)
-    #device = th.device("cuda" if th.cuda().is_available() else "cpu")
+    # experiment.log_parameters(hyper_params) # TODO
     device = th.device("cuda")
     print(device)
-    data = dtst.ImageDataSet(hyper_params['indoor_size'], hyper_params['outdoor_size'])
+    data = dtst.ImageDataSet(hyper_params['train1_size'], hyper_params['train2_size'])
     train_loader = dtst.DataLoader(data, 1, 18)
 
     net = NetArticle().to(device)
-    #net = th.load("weights2.hdf5")
     criterion = nn.MSELoss()
     optimizer = optim.Adam(net.parameters(), lr=hyper_params['learning_rate'])
-    #optimizer = optim.SGD(net.parameters(), lr=hyper_params['learning_rate'])
     losses = train(train_loader, net, criterion, optimizer)
     print(losses)
 
